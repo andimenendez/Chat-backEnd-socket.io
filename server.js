6db2c3c2f6e734405421ba6191d5f14be95e049a
Andrea Menendez <andreaveronicam96@gmail.com>
2023-08-03 02:02:24 -0300

    cambios en config.js

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ import express  from 'express';
 import morgan from 'morgan';
 import http from 'http';
 import {Server as SocketServer} from 'socket.io'
-import cors from 'cors';
+
 import { PORT } from './config.js';
+import cors from 'cors';
+
 const app = express();
 const server = http.createServer(app)
 const io = new SocketServer(server ,{
     cors:{
-        origin:'http://localhost:5173',
+        origin:'*',
     }
 })
 
@@ -17,13 +19,11 @@ app.use(morgan("dev"));
 
 io.on('connection', socket =>{
     const username = socket.handshake.query.username;
-    console.log(username,"connected");
     socket.on('message',(message) =>{
         socket.broadcast.emit('message',{
             body:message,
             from:username
         });
-        console.log(message);
     })
 });
 
